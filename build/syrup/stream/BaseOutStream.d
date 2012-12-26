c237f57cf4363915c67524d243b5395330c84d75
Jonatan Olofsson <jonatan.olofsson@gmail.com>
2012-12-26 20:40:12 +0100

    Removed build dir

--- build/syrup/stream/BaseOutStream.d
+++
@@ -1,7 +0,0 @@
-build/./syrup/stream/BaseOutStream.o: syrup/stream/BaseOutStream.cpp \
- syrup/include/syrup/stream/BaseOutStream.hpp \
- syrup/include/syrup/stream/formatting.hpp
-
-syrup/include/syrup/stream/BaseOutStream.hpp:
-
-syrup/include/syrup/stream/formatting.hpp:
--- build/syrup/stream/formatting.d
+++
@@ -1,4 +0,0 @@
-build/./syrup/stream/formatting.o: syrup/stream/formatting.cpp \
- syrup/include/syrup/stream/formatting.hpp
-
-syrup/include/syrup/stream/formatting.hpp:
